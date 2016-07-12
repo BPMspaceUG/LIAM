a4c0641dc90fc3678a48523c9f7261cc4f7593be
Rob Kuhlig <rob@bpmspace.org>
2016-07-12 13:56:50 +0200

    new connection example for IPMS

---
+++ DB_config/EXAMPLE_login_credentials_DB_bpmspace_ipms.inc.php
@@ -0,0 +1,7 @@
+define("DB_HOST", "localhost"); //change these
+define("DB_USER", "bpmspace_ipms"); // to required DB
+define("DB_PSWD", ""); //connection
+
+define("DB_NAME", "bpmspace_ipms_v1");
+define("DB_TBL", "connections");
+
