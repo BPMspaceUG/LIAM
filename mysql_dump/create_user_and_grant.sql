61eb8e06507f5950cbe5c6144edecc3b24b591a4
Rob Kuhlig <rob@bpmspace.org>
2016-08-03 13:23:20 +0200

    rearange mysql dump files

--- mysql_dump/create_user_and_grant.sql
+++ mysql_dump/create_user_and_grant.sql
@@ -1,5 +1,3 @@
-CREATE USER 'bpmspace_edums'@'localhost' IDENTIFIED BY 'PASSWORTinLASTPASS';
-CREATE USER 'bpmspace_eduapi'@'localhost' IDENTIFIED BY 'PASSWORTinLASTPASS';
-GRANT SELECT, INSERT, UPDATE ON `bpmspace_edums_v4`.* TO 'bpmspace_edums'@'localhost';
-GRANT SELECT ON `bpmspace_edums_v4`.* TO 'bpmspace_eduapi'@'localhost';
+CREATE USER 'secure_login'@'localhost' IDENTIFIED BY 'PASSWORTinLASTPASS';
+GRANT SELECT, INSERT, UPDATE ON `secure_login_v2`.* TO 'secure_login'@'localhost';
 
