bdb6ae3b5d6b62fd9bc1b440ebce9cfe527b8d05
Rob Kuhlig <rob@bpmspace.org>
2017-03-19 09:35:33 +0100

    add new project REPLACER in gitignor and DB_Config

---
+++ DB_config/EXAMPLE_login_credentials_DB_bpmspace_replacer_RO.inc.php
@@ -0,0 +1,9 @@
+<?php
+      $config['db'] =  array(
+        'host' => "localhost:3306",
+        'user' => "bpmspace_repl_RO",
+        'password' => "",
+        'database' => "bpmspace_replacer_v1"
+      );
+
+?>
