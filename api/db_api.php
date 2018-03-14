7b886585f357c76ec70bda807293647be9ec996f
Rob Kuhlig <rob@bpmspace.org>
2018-03-14 11:10:38 +0100

    API min 2

--- api/db_api.php
+++ api/db_api.php
@@ -2723,15 +2723,6 @@ class PHP_CRUD_API {
 // ));
 // $api->executeCommand();
 
-$api = new PHP_CRUD_API(array(
-	'dbengine'=>'MySQL',
-	'hostname'=>'localhost',
-	'username'=>'bpmspace_coms_RO',
-	'password'=>'d85b9220-2d9d-4cfd-9bc8-9a69d3c8d9b3',
-	'database'=>'bpmspace_coms_v1',
-	'charset'=>'utf8mb4'
-));
-$api->executeCommand();
 
 // For Microsoft SQL Server 2012 use:
 
